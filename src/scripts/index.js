69f2a0b33c8664468cc6b4d261d6b71f5149f5d8
Danil Chernov <danilchernov-developer@yandex.ru>
2020-01-16 22:16:18 +0700

    Add at-rules for Header & Intro blocks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,11 @@
 import 'svgxuse';
 import objectFitImages from './polyfills/objectFitImages';
-
-/* custom Modules */
 import sidebar from './modules/sidebar';
 import customRange from './modules/custom-range-slider';
 import map from './modules/map.js';
 import ripple from './modules/ripple-effect-emulation';
 
 objectFitImages();
-
 sidebar();
 customRange();
 map();
